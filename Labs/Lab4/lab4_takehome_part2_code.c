b663704dde385e8959a2475be0baa5e01e097e34
De Camargo Jonas, Mateus <decamar2@illinois.edu>
2019-03-29 16:08:03 -0500

    takehome part 2 code finished, needs comment in first 3 inits, SPI_RX init is commented

--- Labs/Lab4/lab4_takehome_part2_code.c
+++ Labs/Lab4/lab4_takehome_part2_code.c
@@ -16,6 +16,10 @@ chip select each chip when polling
 #include <coecsl.h>
 #include "28335_spi.h"
 #include "user_includes.h"
+/* Here we define several variables. The SPIbyteX are for reading the 5 byte values from the slave chips.
+The SPIencX_reading are used to store the encoder values pulled from the slave chips. SPIenc_state is used
+to determine the state for the state machine in the SPI_RXint ISR. MSP_Reading is used to store data received
+from the MSP in case 5.*/
 long SPIbyte1,SPIbyte2,SPIbyte3,SPIbyte4,SPIbyte5;
 long SPIenc1_reading = 0;
 long SPIenc2_reading = 0;
@@ -36,9 +40,79 @@ FIFO
 */
 void Init_MCP23S08 (unsigned int IODIRvalue, unsigned int IPOLvalue, unsigned int 
 GPPUvalue, unsigned int IOCONvalue, unsigned int OLATvalue){
+	
+	EALLOW;
+		
+		/* Setting the GPAMUX bits 15 and 14 of our 4 pins connected to the slave chips to 0, 
+		thus setting the pins to General Purpose Input/Output mode. GPADIR sets the GPIO pins
+		to act as output, so to transmit data to the master chip. GPASET controls the slave select,
+		setting this value to 1 un-select it stops us from giving it commands. */
+		GpioCtrlRegs.GPAMUX1.bit.GPIO19 = 0;
+		GpioCtrlRegs.GPAMUX1.bit.GPIO48 = 0;
+		GpioCtrlRegs.GPAMUX1.bit.GPIO49 = 0;
+		GpioCtrlRegs.GPAMUX2.bit.GPIO58 = 0;
+
+		GpioCtrlRegs.GPADIR.bit.GPIO19 = 1;
+		GpioCtrlRegs.GPADIR.bit.GPIO48 = 1;
+		GpioCtrlRegs.GPADIR.bit.GPIO49 = 1;
+		GpioCtrlRegs.GPADIR.bit.GPIO58 = 1;
+
+		GpioDataRegs.GPASET.bit.GPIO19 = 1;
+		GpioDataRegs.GPASET.bit.GPIO48 = 1;
+		GpioDataRegs.GPASET.bit.GPIO49 = 1;
+		GpioDataRegs.GPASET.bit.GPIO58 = 1;
+
+	EDIS;
+
+	InitSpiaGpio();
+	
+	/*Setting SPI config control register. SWReset sets the SPI ready to receive/transmit next char.
+	Setting Pol and Phase so that data data is recorded on the rising edge, default low. 
+	Then we control the length of chars sent over SPI to 8 bits. We then configure the SPI as a master,
+	also enabling it for transmission. The SPI interrupt flag is also enabled. Finally we configure 
+	the baud rate for the SPI connection. 
+	*/
+	SpiaRegs.SPICCR.bit.SPISWRESET = 0;  // Put SPI in reset
+
+	SpiaRegs.SPICCR.bit.CLKPOLARITY = 0; 
+	SpiaRegs.SPICTL.bit.CLK_PHASE = 1;
+
+	SpiaRegs.SPICCR.bit.SPICHAR = 7;   // set to transmitt 8 bits
+
+	SpiaRegs.SPICTL.bit.MASTER_SLAVE = 1;
+	SpiaRegs.SPICTL.bit.TALK = 1;
+
+	SpiaRegs.SPICTL.bit.SPIINTENA = 0;
+
+	SpiaRegs.SPISTS.all=0x00E0;
+
+	SpiaRegs.SPIBRR = 39;   // divide by 40 2.5 Mhz
+	
+	/* The SPIFFTX register controls the First In First Out Transmit Buffer. SPIRST set to 1 allows
+	the SPI FIFO to resume normal transmit and receive operation. SPIFFENA set to 1 enables the FIFO,
+	so if disabled the FIFO would not be used. TXFIFO resets the FIFO pointer to 0, clearing any past data. 
+	The TXFFINTCLR set to 1 clears the interrupt flag for the Transmit buffer. The same is done bellow for
+	the Receive buffer registers to prepare for communication. */
+	SpiaRegs.SPIFFTX.bit.SPIRST = 1;
+	SpiaRegs.SPIFFTX.bit.SPIFFENA = 1;
+	SpiaRegs.SPIFFTX.bit.TXFIFO = 0;
+	SpiaRegs.SPIFFTX.bit.TXFFINTCLR = 1;
+
+	SpiaRegs.SPIFFRX.bit.RXFIFORESET = 0;
+	SpiaRegs.SPIFFRX.bit.RXFFOVFCLR = 1;
+	SpiaRegs.SPIFFRX.bit.RXFFINTCLR = 1;
+	SpiaRegs.SPIFFRX.bit.RXFFIENA = 0;
+
+	SpiaRegs.SPIFFCT.all=0x00;
+
+	SpiaRegs.SPICCR.bit.SPISWRESET = 1;  // Pull the SPI out of reset
+
+	SpiaRegs.SPIFFTX.bit.TXFIFO=1;
+	SpiaRegs.SPIFFRX.bit.RXFIFORESET=1;
+	
 	//write 1 byte data of IODIRvalue to Direction register
 	//pull chip select low for chip23S08
-	GpioDataRegs.GPACLEAR.bit.GPIO19 = 1;
+	GpioDataRegs.GPACLEAR.bit.GPIO19 = 1;	
 	//send 3 bytes of data across
 	SpiaRegs.SPITXBUF = ((unsigned)0x40)<<8;  // WR MODE A1 A0: 0 0 
 	SpiaRegs.SPITXBUF = ((unsigned)0x00)<<8;  //Address of IODIR register: 00h
@@ -179,20 +253,29 @@ transmit and receive have finished
 */
 void SPI_RXint(void) {
 
-	//pull all chip select high
-	GpioDataRegs.GPASET.bit.GPIO9 = 1;
-	GpioDataRegs.GPASET.bit.GPIO10 = 1;
-	GpioDataRegs.GPASET.bit.GPIO11 = 1;
-	GpioDataRegs.GPASET.bit.GPIO22 = 1;
+	/* Setting all the GPIOX pins slave select 1, un-selecting them, so that we can call the 
+	specific one needed in each state case in the state machine bellow, as we read one at a time. */
 	GpioDataRegs.GPASET.bit.GPIO19 = 1;
+	GpioDataRegs.GPASET.bit.GPIO48 = 1;
+	GpioDataRegs.GPASET.bit.GPIO49 = 1;
+	GpioDataRegs.GPASET.bit.GPIO58 = 1;
 
 	switch (SPIenc_state) {
 		case 1:
+			/*First we read the first byte from the receive buffer, this is random data. Then we define
+			for the interrupt flag of the receive buffer to be set after 5 bytes have been received. 
+			We also progress the state of the machine to read the data from the next GPIOX pins. 
+			Then we clear the GPIO48,49, and 58 register bits. When these are are pulled low, the DEMUX
+			pulls the first output low also, allowing us to receive data from it. Then we send set 
+			of instructions to the transmit buffer, select output register, then clear it. 
+			We must send bytes in order to be able to receive at the same time, so we send all 0s. 
+			*/
 			SPIbyte1 = SpiaRegs.SPIRXBUF;
-
 			SpiaRegs.SPIFFRX.bit.RXFFIL = 5;
 			SPIenc_state = 2;
-			GpioDataRegs.GPACLEAR.bit.GPIO9 = 1;
+			GpioDataRegs.GPASET.bit.GPIO48 = 1;
+			GpioDataRegs.GPASET.bit.GPIO49 = 1;
+			GpioDataRegs.GPASET.bit.GPIO58 = 1;
 			SpiaRegs.SPITXBUF = ((unsigned)0x68)<<8;
 			SpiaRegs.SPITXBUF = 0;
 			SpiaRegs.SPITXBUF = 0;
@@ -201,16 +284,25 @@ void SPI_RXint(void) {
 
 			break;
 		case 2:
+			/*First we read the first byte of random data from the receiver buffer, followed by the next 4 bytes.
+			The transmit buffer value is masked before saving to our variable so that we only save the LSB.
+			We must read the bytes individually since the SPI works as a FIFO register. We then concatenate all the 
+			bytes (without the random data) into our own variable for encoder 1. We set the interrupt flag for the 
+			receiver buffer to be activated after 5 bytes are received. Then progress the state to read the next encoder
+			value in the next time around. We then clear the GPIO49 and 58 registers. When these are are pulled low, 
+			the DEMUX pulls the second output low also, allowing us to receive data from it. Finally we send a set of
+			instructions to clear the output register followed by 0s to fill in the remaining 4 bytes.
+			*/
 			SPIbyte1 = SpiaRegs.SPIRXBUF;
 			SPIbyte2 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIbyte3 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIbyte4 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIbyte5 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIenc1_reading = (SPIbyte2<<24) | (SPIbyte3<<16) | (SPIbyte4<<8) | SPIbyte5;
-
 			SpiaRegs.SPIFFRX.bit.RXFFIL = 5;
 			SPIenc_state = 3;
-			GpioDataRegs.GPACLEAR.bit.GPIO10 = 1;
+			GpioDataRegs.GPASET.bit.GPIO49 = 1;
+			GpioDataRegs.GPASET.bit.GPIO58 = 1;
 			SpiaRegs.SPITXBUF = ((unsigned)0x68)<<8;
 			SpiaRegs.SPITXBUF = 0;
 			SpiaRegs.SPITXBUF = 0;
@@ -219,16 +311,25 @@ void SPI_RXint(void) {
 
 			break;
 		case 3:
+			/*First we read the first byte of random data from the receiver buffer, followed by the next 4 bytes.
+			The transmit buffer value is masked before saving to our variable so that we only save the LSB.
+			We must read the bytes individually since the SPI works as a FIFO register. We then concatenate all the 
+			bytes (without the random data) into our own variable for encoder 1. We set the interrupt flag for the 
+			receiver buffer to be activated after 5 bytes are received. Then progress the state to read the next encoder
+			value in the next time around. We then clear the GPIO48 and 58 registers. When these are are pulled low, 
+			the DEMUX pulls the third output low also, allowing us to receive data from it. Finally we send a set of
+			instructions to clear the output register followed by 0s to fill in the remaining 4 bytes.
+			*/
 			SPIbyte1 = SpiaRegs.SPIRXBUF;
 			SPIbyte2 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIbyte3 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIbyte4 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIbyte5 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIenc2_reading = (SPIbyte2<<24) | (SPIbyte3<<16) | (SPIbyte4<<8) | SPIbyte5;
-
 			SpiaRegs.SPIFFRX.bit.RXFFIL = 5;
 			SPIenc_state = 4;
-			GpioDataRegs.GPACLEAR.bit.GPIO11 = 1;
+			GpioDataRegs.GPASET.bit.GPIO48 = 1;
+			GpioDataRegs.GPASET.bit.GPIO58 = 1;
 			SpiaRegs.SPITXBUF = ((unsigned)0x68)<<8;
 			SpiaRegs.SPITXBUF = 0;
 			SpiaRegs.SPITXBUF = 0;
@@ -237,16 +338,24 @@ void SPI_RXint(void) {
 
 			break;
 		case 4:
+			/*First we read the first byte of random data from the receiver buffer, followed by the next 4 bytes.
+			The transmit buffer value is masked before saving to our variable so that we only save the LSB.
+			We must read the bytes individually since the SPI works as a FIFO register. We then concatenate all the 
+			bytes (without the random data) into our own variable for encoder 1. We set the interrupt flag for the 
+			receiver buffer to be activated after 5 bytes are received. Then progress the state to read the next encoder
+			value in the next time around. We then clear the GPIO58 register. When these are are pulled low, 
+			the DEMUX pulls the fourth output low also, allowing us to receive data from it. Finally we send a set of
+			instructions to clear the output register followed by 0s to fill in the remaining 4 bytes.
+			*/
 			SPIbyte1 = SpiaRegs.SPIRXBUF;
 			SPIbyte2 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIbyte3 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIbyte4 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIbyte5 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIenc3_reading = (SPIbyte2<<24) | (SPIbyte3<<16) | (SPIbyte4<<8) | SPIbyte5;
-
 			SpiaRegs.SPIFFRX.bit.RXFFIL = 5;
 			SPIenc_state = 5;
-			GpioDataRegs.GPACLEAR.bit.GPIO22 = 1;
+			GpioDataRegs.GPASET.bit.GPIO58 = 1;
 			SpiaRegs.SPITXBUF = ((unsigned)0x68)<<8;
 			SpiaRegs.SPITXBUF = 0;
 			SpiaRegs.SPITXBUF = 0;
@@ -255,13 +364,21 @@ void SPI_RXint(void) {
 
 			break;
 		case 5:
+			/*First we read the first byte of random data from the receiver buffer, followed by the next 4 bytes.
+			The transmit buffer value is masked before saving to our variable so that we only save the LSB.
+			We must read the bytes individually since the SPI works as a FIFO register. We then concatenate all the 
+			bytes (without the random data) into our own variable for encoder 1. We set the interrupt flag for the 
+			receiver buffer to be activated after 5 bytes are received. Then progress the state to read the next encoder
+			value in the next time around. We then clear the GPIO19 register. When this is pulled low, 
+			it allows us to receive data from the MCP. Finally we send a set of
+			instructions to clear the output register followed by 0s to fill in the remaining 4 bytes.
+			*/
 			SPIbyte1 = SpiaRegs.SPIRXBUF;
 			SPIbyte2 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIbyte3 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIbyte4 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIbyte5 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIenc4_reading = (SPIbyte2<<24) | (SPIbyte3<<16) | (SPIbyte4<<8) | SPIbyte5;
-
 			SpiaRegs.SPIFFRX.bit.RXFFIL = 3;//MCP23S08 sends back 3 bytes of data, i.e., 3 interrupt
 			SPIenc_state = 8;//read the 8 bit GPIO register after the four LS7366 chips have been read
 			GpioDataRegs.GPACLEAR.bit.GPIO19 = 1;//chip select MCP23S08, pull low to select
@@ -272,6 +389,7 @@ void SPI_RXint(void) {
 			break;
 			
 		case 8:
+			//reading in values from last device, then posting SWI to resume previous code
 			SPIbyte1 = SpiaRegs.SPIRXBUF;
 			SPIbyte2 = SpiaRegs.SPIRXBUF & 0xFF;
 			SPIbyte3 = SpiaRegs.SPIRXBUF & 0xFF; 
