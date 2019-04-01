27c42d463670f35b18da750a3964a97639134b08
Zhou, Lance <hzhou39@illinois.edu>
2019-04-01 07:52:04 -0500

    Update lab4_takehome_part2_code.c

--- Labs/Lab4/lab4_takehome_part2_code.c
+++ Labs/Lab4/lab4_takehome_part2_code.c
@@ -10,7 +10,7 @@ GPIO17:SPISOMIA
 GPIO18:SPICLKA
 GPIO19, GPIO48, GPIO49, GPIO58:General Purpose I/O pin
 chip select each chip when polling
-	pull low to select, pull back high after done
+pull low to select, pull back high after done
 */
 #include <tistdtypes.h>
 #include <coecsl.h>
@@ -26,7 +26,7 @@ long SPIenc2_reading = 0;
 long SPIenc3_reading = 0;
 long SPIenc4_reading = 0;
 int SPIenc_state = 0;
-int	SPIenc_state_errors = 0;
+int SPIenc_state_errors = 0;
 long MSP_reading = 0;
 
 unsigned int dac1data = 0;
@@ -34,9 +34,6 @@ unsigned int dac2data = 0;
 /*
 initialize the MCP23S08 chip depending on what parameters are passed. Poll on the RXFFST status bits
 to determine when the SPI transmit and receive have completed before returning from this function. 
-
-write 1 byte data of IODIRvalue to Direction register of MCP23S08 through SPI by writing into transmit
-FIFO
 */
 void Init_MCP23S08 (unsigned int IODIRvalue, unsigned int IPOLvalue, unsigned int 
 GPPUvalue, unsigned int IOCONvalue, unsigned int OLATvalue){
